f5dbf1a2344c69f64a9945f148106d516a31ea7f
faisalkabir198 <faisalkabir.eee@gmail.com>
2016-09-16 14:55:49 -0500

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,11 @@
+temp<-"datafile.txt"
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", temp,method="libcurl")
+data<-read.table(unz(temp,"household_power_consumption.txt"),sep=";",header=TRUE)
+data[] <- lapply(data, as.character)
+data$asDate<- as.Date(data$Date,format="%d/%m/%Y")
+subdata<- subset(data,asDate>="2007-02-01"& asDate<="2007-02-02")
+subdata$DT <- paste(subdata$Date, subdata$Time, sep=" ")
+subdata$asDT <- strptime(subdata$DT, format="%d/%m/%Y %H:%M:%S")
+png("plot2.png",  width = 480, height = 480)
+with(subdata,plot(subdata$asDT,subdata$Global_active_power,type="l",ylab="Global Active Power (kilowatts)", xlab=""))
+dev.off()
